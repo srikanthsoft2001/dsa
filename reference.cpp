212a72bb887074d78cbe23da9e4e6ac5deb49fc9
Srikanth Mahenderkar <rmsrikanth25@gmail.com>
2026-01-10 10:02:29 +0530

    references -end

--- reference.cpp
+++ reference.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
+    int a = 10;
+    int &r = a; // initialized at declaration
+    // r is not taking any memory - doesn't consume memory
 
+    int b = 25;
+    r = b;
+
+    cout << a << endl;
+    cout << ++r << endl;
+    cout << a << endl;
     return 0;
 }
